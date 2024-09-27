720781e0933c4a759f1d04f453e27bf6a0f4465b
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-09-27 14:43:40 +0530

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: false,
+  },
+  overrides: [{ files: ['*.js'] }],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: ['airbnb-base', 'plugin:unicorn/recommended', 'prettier'],
+  ignorePatterns: ['*', '!src', '!src/**/*'],
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'unicorn/better-regex': 'warn',
+    'unicorn/numeric-separators-style': [
+      'error',
+      { number: { minimumDigits: 0 } },
+    ],
+    'no-plusplus': 'off',
+    'import/extensions': ['error', 'ignorePackages'],
+    'no-param-reassign': ['error', { props: false }],
+
+    'lines-between-class-members': [
+      'error',
+      {
+        enforce: [
+          { blankLine: 'always', prev: '*', next: 'method' },
+          { blankLine: 'always', prev: 'method', next: '*' },
+          { blankLine: 'never', prev: 'field', next: 'field' },
+        ],
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['**/*.test.js'],
+      env: {
+        jest: true,
+      },
+      extends: ['plugin:jest/recommended'],
+      plugins: ['jest'],
+    },
+  ],
+};

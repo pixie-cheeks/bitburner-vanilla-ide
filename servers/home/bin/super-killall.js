ab211e51bf410fa23d4740b1f8c7ed890813dc12
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-09-30 18:31:27 +0530

    Add success indication for superKillall

--- servers/home/bin/super-killall.js
+++ servers/home/bin/super-killall.js
@@ -2,9 +2,13 @@ import flatHostnamesList from '../data/flat-hostnames-list.js';
 
 /** @param {NS} ns */
 const superKillAll = (ns) => {
-  flatHostnamesList.forEach(
-    (hostname) => ns.ps(hostname).length > 0 && ns.killall(hostname),
-  );
+  flatHostnamesList.forEach((hostname) => {
+    const processes = ns.ps(hostname);
+    if (processes.length === 0) return;
+
+    ns.killall(hostname);
+    ns.tprint(`\n\nOn ${hostname}, killed:\n${JSON.stringify(processes)}\n\n`);
+  });
 };
 
 const main = superKillAll;

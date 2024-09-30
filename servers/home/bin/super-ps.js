2f0ad3a2c7c3261bfbbc9201d88d1868349a9b23
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-09-30 19:36:24 +0530

    Write superPs for convenience

---
+++ servers/home/bin/super-ps.js
@@ -0,0 +1,27 @@
+import flatHostnamesList from '../data/flat-hostnames-list.js';
+
+const createMessage = (hostname, { filename, threads, pid, args }) =>
+  `(T - ${threads}) (PID - ${pid}) ${hostname}:${filename} ${args.join(' ')}`;
+
+/** @param {NS} ns */
+const superPs = (ns) => {
+  const messages = [];
+
+  flatHostnamesList.forEach((hostname) => {
+    const processes = ns.ps(hostname);
+    if (processes.length === 0) return;
+
+    processes.forEach((process) =>
+      messages.push(createMessage(hostname, process)),
+    );
+  });
+
+  if (messages.length === 0) {
+    ns.tprint('No processes running in non-pservers.');
+  } else {
+    ns.tprint(`\n${messages.join('\n')}`);
+  }
+};
+
+const main = superPs;
+export { main, superPs };

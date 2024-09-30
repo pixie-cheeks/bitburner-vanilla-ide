8fef5115b9b50b3bbc3b2749a0a4e8802483ca33
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-09-30 17:44:02 +0530

    Explicitly list all portopener methods
    This is to avoid a mechanism that prevents players from bypassing
    ram usage calculation. Will need to look into how to only load
    a method when you need it without avoiding this mechanism in the future.

--- servers/home/ns-utils/nuke-server.js
+++ servers/home/ns-utils/nuke-server.js
@@ -1,18 +1,18 @@
 import repeat from '../utils/repeat.js';
 
-const portOpeners = [
-  { methodName: 'brutessh' },
-  { methodName: 'ftpcrack' },
-  { methodName: 'relaysmtp' },
-  { methodName: 'httpworm' },
-  { methodName: 'sqlinject' },
-];
-
 /** @param {NS} ns */
 const nukeServer = (ns, hostname) => {
-  repeat(ns.getServerNumPortsRequired(hostname), (i) => {
-    ns[portOpeners[i].methodName](hostname);
-  });
+  const portOpeners = [
+    ns.brutessh,
+    ns.ftpcrack,
+    ns.relaysmtp,
+    ns.httpworm,
+    ns.sqlinject,
+  ];
+
+  repeat(ns.getServerNumPortsRequired(hostname), (i) =>
+    portOpeners[i](hostname),
+  );
 
   ns.nuke(hostname);
 };
